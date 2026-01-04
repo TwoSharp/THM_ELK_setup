91bb652abeb453310070e73ce5d949c867fd9338
Landon <87420106+TwoSharp@users.noreply.github.com>
2026-01-04 15:29:16 -0700

    ELK_setup.sh

--- ELK_setup.sh
+++ ELK_setup.sh
@@ -17,7 +17,7 @@ done
 echo "done installing, now editing config files..."
 
 # kibana config 
-cd /etc/kibana && sed -i 's/#server.port:/server.port/g' kibana.yml && sed -i 's/#server.host: "localhost"/server.host: 0.0.0.0/g' kibana.yml
+cd /etc/kibana && sed -i 's/#server.port:/server.port:/g' kibana.yml && sed -i 's/#server.host: "localhost"/server.host: 0.0.0.0/g' kibana.yml
 
 # logstash config
 cd /etc/logstash && sed -i 's/# config.reload.interval:/config.reload.interval:/g' logstash.yml && sed -i 's/# config.reload.automatic: false/config.reload.automatic: true/g' logstash.yml
@@ -25,10 +25,10 @@ cd /etc/logstash && sed -i 's/# config.reload.interval:/config.reload.interval:/
 # elasticsearch config
 cd /etc/elasticsearch && sed -i 's/#network.host: 192.168.0.1/network.host: 127.0.0.1/g' elasticsearch.yml && sed -i 's/#http.port:/http.port:/g' elasticsearch.yml
 
-echo "done configuring config files, restarting services..."
+echo "done editing config files, restarting services..."
 
 for k in $e $l $k; do
 	systemctl restart $k
 done
 
-echo "done"
+echo "services restarted"
